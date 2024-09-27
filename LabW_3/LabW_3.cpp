b2c0bf7564f327529affe620bd8662fcccb822d0
Shurikovdanik <shurikovdanik1@mail.ru>
2024-09-27 17:07:39 +0300

    Update LabW_3.cpp

--- LabW_3/LabW_3.cpp
+++ LabW_3/LabW_3.cpp
@@ -2,39 +2,31 @@
 bool checkInput(double **a, int height) // проверка на соблюдение условия a[i][j] = a[j][i]
 {
     bool isSuitable = 1;
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < height; j++)
-        {
-            if (a[i][j] - a[j][i] < -0.0001 || a[i][j] - a[j][i] > 0.0001)
-            {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < height; j++) {
+            if (a[i][j] - a[j][i] < -0.0001 || a[i][j] - a[j][i] > 0.0001) {
                 isSuitable = 0;
             }
         }
     }
     return isSuitable;
 }
-bool input(double **a, int height) //ввод значений в матрицу height x height. Возвращает 0 <=> введённые с консоли значения не подпадаю под условие a[i][j] = a[j][i]
+bool input(double **a, int height) // ввод значений в матрицу height x height. Возвращает 0 <=> введённые с консоли значения не подпадаю под условие a[i][j] = a[j][i]
 {
     bool isAuto, isCorrect;
     isCorrect = 1;
-    std::cout << std::endl
-              << "0 to manually fill array, 1 to fill it with random numbers: ";
+    std::cout << std::endl << "0 to manually fill array, 1 to fill it with random numbers: ";
     std::cin >> isAuto;
     if (isAuto) // Генерация случайных чисел (в описанных пределах. До 3 знаков после запятой включительно)
     {
         srand(static_cast<unsigned int>(time(0)));
         double min, max;
-        std::cout << std::endl
-                  << "Minimum and maximum possible numbers: ";
+        std::cout << std::endl << "Minimum and maximum possible numbers: ";
         std::cin >> min >> max;
-        for (int i = 0; i < height; i++)
-        {
-            for (int j = 0; j < height; j++)
-            {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < height; j++) {
                 a[i][j] = (rand() % (int)(max * 1000 - min * 1000) + (int)min * 1000) / 1000.0;
-                if (i > j)
-                {
+                if (i > j) {
                     a[i][j] = a[j][i];
                 }
                 std::cout << a[i][j] << "\t";
@@ -43,43 +35,38 @@ bool input(double **a, int height) //ввод значений в матрицу
             std::cout << std::endl;
         }
     }
-    else
-    {
-        for (int i = height - 1; i > 0; i++)
-        { // ручной ввод с консоли
-            for (int j = 0; j <= i; j++)
-            {
-                std::cin >> a[i][j];
-                a[j][i] = a[i][j];
-                std::cout << "/t";
+    else {
+        for (int i = 0; i < height; i++) { // ручной ввод с консоли
+            std::cout << i + 1 << " string: ";
+            for (int j = 0; j < height; j++) {
+                if (i >= j) {
+                    std::cin >> a[i][j];
+                    a[j][i] = a[i][j];
+                    std::cout << " ;";
+                }
             }
             std::cout << std::endl;
         }
     }
-    if (!checkInput(a, height))
-    {
+    if (!checkInput(a, height)) {
         std::cout << "ERROR";
         isCorrect = 0;
     }
     return isCorrect;
 }
-void replace(double **a, int x1, int y1, int x2, int y2) //перемена местами двух значений в матрице с соблюдением условия a[i][j] = a[j][i]
+void replace(double **a, int x1, int y1, int x2, int y2) // перемена местами двух значений в матрице с соблюдением условия a[i][j] = a[j][i]
 {
     double buffer = a[x1][y1];
     a[x1][y1] = a[x2][y2];
     a[x2][y2] = buffer;
 }
-void sort(double **a, int height) //сортировка матрицы таким образом: наибольший значения матрицы находятся на главной диагонали, с верхнего левого угла к правому нижнему они расположены по убыванию
+void sort(double **a, int height) // сортировка матрицы таким образом: наибольший значения матрицы находятся на главной диагонали, с верхнего левого угла к правому нижнему они расположены по убыванию
 {
     std::cout << std::endl;
-    for (int i = 0; i < height; i++) // номер на главной диагонали
-    {
-        for (int j = 0; j < height; j++) // строка
-        {
-            for (int k = 0; k < height; k++) // столбец
-            {
-                if (a[i][i] < a[j][k] && ((j != k) || (j > i)))
-                {
+    for (int i = 0; i < height; i++) /* номер на главной диагонали */ {
+        for (int j = 0; j < height; j++) /* строка */ {
+            for (int k = 0; k < height; k++) /* столбец */ {
+                if (a[i][i] < a[j][k] && ((j != k) || (j > i))) {
                     replace(a, i, i, j, k);
                 }
             }
@@ -88,10 +75,8 @@ void sort(double **a, int height) //сортировка матрицы таки
 }
 void cout(double **a, int height) // вывод матрицы на консоль
 {
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < height; j++)
-        {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < height; j++) {
             std::cout << a[i][j] << "\t";
         }
         std::cout << std::endl;
@@ -102,23 +87,18 @@ int search(double **a, int height) // поиск первой строки с т
     bool isNegative = 0;
     bool isConstantlyNegative = 1;
     int res = -1;
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < height; j++)
-        {
-            if (a[i][j] < 0)
-            {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < height; j++) {
+            if (a[i][j] < 0) {
                 isNegative = 1;
             }
-            else if (isNegative == 1)
-            {
+            else if (isNegative == 1) {
                 isNegative = 0;
                 isConstantlyNegative = 0;
                 break;
             }
         }
-        if (isNegative && isConstantlyNegative)
-        {
+        if (isNegative && isConstantlyNegative) {
             res = i + 1;
             break;
         }
@@ -133,18 +113,14 @@ int main()
     std::cin >> height;
     std::cout << std::endl;
     double **numbers = new double *[height];
-    for (int i = 0; i < height; i++)
-    {
+    for (int i = 0; i < height; i++) {
         numbers[i] = new double[height];
     }
-    if (input(numbers, height))
-    {
-        if (search(numbers, height) != -1)
-        {
+    if (input(numbers, height)) {
+        if (search(numbers, height) != -1) {
             std::cout << search(numbers, height) << " is the first line with only negative numbers. " << std::endl;
         }
-        else
-        {
+        else {
             std::cout << "There is no line with only negative numbers";
         }
         sort(numbers, height);
