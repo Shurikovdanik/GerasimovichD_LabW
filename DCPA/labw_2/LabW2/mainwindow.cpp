0c60bbff9ed61060b4b456497c2b654d08fb49c0
Shurikovdanik <shurikovdanik1@mail.ru>
2025-02-26 20:46:12 +0300

    Start of ui

--- DCPA/labw_2/LabW2/mainwindow.cpp
+++ DCPA/labw_2/LabW2/mainwindow.cpp
@@ -1,13 +1,48 @@
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QRegularExpression>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include "MainL.h"
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-}
+    QAction* openAction = new QAction(tr("&Open"), this);
+    QAction* saveAction = new QAction(tr("&Save"), this);
+    QAction* task170 = new QAction(tr("&Process Text"), this);
+    QAction* task182 = new QAction(tr("&Process Text"), this);
+    QAction* task194 = new QAction(tr("&Process Text"), this);
 
+    connect(saveAction, &QAction::triggered, this, &Ui_MainWindow::saveFile);
+    connect(task170, &QAction::triggered, this, &Ui_MainWindow::Button170);
+    connect(task182, &QAction::triggered, this, &Ui_MainWindow::Button182);
+    connect(task194, &QAction::triggered, this, &Ui_MainWindow::Button194);
+}
+void MainWindow::saveFile() {
+    QString fileName = QFileDialog::getSaveFileName(this, tr("Save File"), "", tr("Text Files (*.txt);;All Files (*)"));
+    if (fileName.isEmpty())
+        return;
+
+    QFile file(fileName);
+    if (!file.open(QIODevice::WriteOnly)) {
+        QMessageBox::warning(this, tr("Error"), tr("Cannot save file."));
+        return;
+    }
+
+    QTextStream out(&file);
+    out << text->text();
+    file.close();
+}
+void MainWindow::Button170() {
+    MainL res;
+    text->setText(res.ex170(textEdit->toPlainText()));
+}
+void MainWindow::task170 () {
+
+}
 MainWindow::~MainWindow()
 {
     delete ui;
--- DCPA/labw_2/LabW2/mainwindow.h
+++ DCPA/labw_2/LabW2/mainwindow.h
@@ -1,8 +1,9 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include <QTextEdit>
 #include <QMainWindow>
-
+#include <QPushButton>
+#include <QLabel>
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
@@ -12,12 +13,23 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
+private slots:
+    void openFile();
+    void saveFile();
+    void task170();
+    void task182();
+    void task194();
 private:
     Ui::MainWindow *ui;
+    QLabel* text;
+    QTextEdit* textEdit;
+    QPushButton* Button170;
+    QPushButton* Button182;
+    QPushButton* Button194;
+    QPushButton* saveButton;
+    QPushButton* processButton;
 };
 #endif // MAINWINDOW_H
